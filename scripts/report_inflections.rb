cdaa9b2e84eccbba8ff8f5e8ad515c3270baeea9
Ben Crowell <notreal@fakewillnotwork.invalid>
2021-11-04 16:30:32 -0700

    work on report_inflections.rb

--- scripts/report_inflections.rb
+++ scripts/report_inflections.rb
@@ -8,11 +8,27 @@ require 'json'
 
 # https://github.com/cltk/greek_treebank_perseus
 $fields = ["pos","person","number","tense","mood","voice","gender","case","degree"]
+$all_values = {'person'=>['1','2','3'],'number'=>['s','d','p'],'tense'=>['p','i','r','l','t','f','a'],'mood'=>['i','s','o','n','m','p'],
+           'voice'=>['a','p','m','e']}
 
 # usage:
-#   report_inflections.rb καλεω person=3 mood=i voice=a tense=p,i,a number=s,p
+#   report_inflections.rb καλεω person=3 mood=* voice=a tense=p,i,a number=s,p
 # The rightmost key changes most rapidly, as expected intuitively from the way we write decimal notation.
 # The first arg is the lemma. Accents in the lemma are ignored.
+# Using * causes results to be broken down by that key. If you just don't care about that key and want to accept all its possible
+# values, then simply don't include the key on the command line.
+
+=begin
+  Typical database entry:
+  "ἀγείρετο": [
+    "ἀγείρω",
+    "1",
+    "v3siie---",
+    1, <--- number of occurrences
+    false,
+    null
+  ],
+=end
 
 def main
   indentation_spacing = 2
@@ -26,7 +42,13 @@ def main
   selector_strings.each { |s|
     key,val = s.split(/=/)
     keys.push(key)
-    values.push(val.split(/,/))
+    if val=='*' then
+      vv = $all_values[key]
+      if vv.nil? then print "* not implemented for key=#{key}\n"; exit(-1) end
+    else
+      vv = val.split(/,/)
+    end
+    values.push(vv)
   }
   nvals = values.map { |x| x.length}
   n = nvals.length
@@ -42,17 +64,9 @@ def main
   last_odo = Array.new(n, -1)
   0.upto(total_odometer_values-1) { |o|
     odo = count_to_odometer(o,nvals)
-    indent = 0
-    0.upto(n-1) { |i|
-      if last_odo[i]!=odo[i] && nvals[i]!=1 then
-        print " "*indent*indentation_spacing,describe_tag(keys[i],values[i][odo[i]]),"\n"
-      end
-      if nvals[i]!=1 then
-        indent +=1 
-      end
-    }
     #print "odo=#{odo}\n"
     matches = []
+    n_occurrences = []
     all_the_same_pos = ''
     homer_filtered.each_pair { |inflected,data|
       values_for_this_form = data[2].chars
@@ -67,10 +81,26 @@ def main
       part_of_speech = values_for_this_form[0] # e.g., 'v' if it's a verb
       all_the_same_pos = all_the_same_pos+part_of_speech
       matches.push(inflected)
+      n_occurrences.push(data[3])
     }
-    if all_the_same_pos=~/^v+$/ then matches=deredundantize_verb(matches) end # all verbs
-    if matches.length>=1 then results=matches.join(',') else results='-' end
-    print " "*n_varying*indentation_spacing,results,"\n"
+    n_matches = matches.length
+
+    indent = 0
+    changed_past_here = false
+    0.upto(n-1) { |i|
+      if last_odo[i]!=odo[i] && nvals[i]!=1 then changed_past_here = true end
+      next if nvals[i]==1
+      if changed_past_here && ! (n_matches==0 && i==n-1) then
+        print " "*indent*indentation_spacing,describe_tag(keys[i],values[i][odo[i]]),"\n"
+      end
+      indent +=1 
+    }
+
+    if all_the_same_pos=~/^v+$/ then matches,n_occurrences=deredundantize_verb([matches,n_occurrences]) end # all verbs
+    if n_matches>=1 then
+      results=0.upto(matches.length-1).map { |i| "#{matches[i]} (#{n_occurrences[i]})"}.join(', ')
+      print " "*n_varying*indentation_spacing,results,"\n"
+    end
     last_odo = odo
   }
 end
@@ -87,23 +117,35 @@ def count_to_odometer(count,nvals)
   return odo
 end
 
-def deredundantize_verb(l)
-  if l.length<2 then return l end
+def deredundantize_verb(x)
+  l,n_occurrences = x
+  if l.length<2 then return [l,n_occurrences] end
   0.upto(l.length-1) { |i|
     0.upto(l.length-1) { |j|
       next if i==j
-      if remove_accents(l[i])==remove_accents(l[j]+"ν") then return deredundantize_verb(l-[l[j]]) end
-      if remove_accents(l[i])==remove_accents("ε"+l[j]) then return deredundantize_verb(l-[l[j]]) end
+      if remove_accents(l[i])==remove_accents(l[j]+"ν") then return deredundantize_verb(deredundantize_helper(l,n_occurrences,l[j])) end
+      if remove_accents(l[i])==remove_accents("ε"+l[j]) then return deredundantize_verb(deredundantize_helper(l,n_occurrences,l[j])) end
       if l[j]=~/(.*)᾽/ then
         stem = $1
         m = stem.length
-        if l[i][0,m]==stem then return deredundantize_verb(l-[l[j]]) end
+        if l[i][0,m]==stem then return deredundantize_verb(deredundantize_helper(l,n_occurrences,l[j])) end
       end
     }
   }
-  return l
+  return [l,n_occurrences]
 end
 
+def deredundantize_helper(array1,array2,value_to_delete)
+  i = array1.index(value_to_delete)
+  if i.nil? then return [array1,array2] end
+  a1 = array1.dup
+  a2 = array2.dup
+  a1.delete_at(i)
+  a2.delete_at(i)
+  return [a1,a2]
+end
+
+# https://github.com/cltk/greek_treebank_perseus
 def describe_tag(tag,value)
   if tag=='tense' then
     if value=='p' then return 'present' end
