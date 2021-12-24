6a853e2859254b9a58268bc779030e9a6f95cd45
Ben Crowell <notreal@fakewillnotwork.invalid>
2021-12-24 07:53:52 -0800

    tests for Writing.phoneticize

--- greek/writing.rb
+++ greek/writing.rb
@@ -1,11 +1,36 @@
 class Writing
+  # ruby -e "require './greek/writing.rb'; require './lib/string_util.rb'; print Writing.phoneticize('ῥέω')"
+  # ruby -e "require './greek/writing.rb'; require './lib/string_util.rb'; Writing.test_phoneticize"
+  def Writing.test_phoneticize()
+    tests = [
+      ["ῥέω","hρε!ω"],
+      ["ῥηΐδιος","hρηι!διοσ"],
+      ["δαμᾷ","δαμα~ι"],
+      ["μὲν","με!ν"],
+      ["ἀγλαός","αγλαο!σ"],
+      ["ἑλώριον","hελω!ριον"]
+    ]
+    tests.each { |x|
+      inp,out = x
+      result = Writing.phoneticize(inp)
+      if result!=out then
+        $stderr.print "test failed, in=#{inp} out=#{result} expected=#{out}\n"
+        exit(-1)
+      else
+        print "        passed: #{inp} #{result}\n"
+      end
+    }
+  end
   def Writing.phoneticize(s)
-    # decompose iota subscript into a vowel plus an iota
+    # Turn a Greek string into a phoneticized version that works better with
+    # algorithms like longest common subsequence.
     s = s.downcase
     s = grave_to_acute(s) # we don't care about phonetic differences that only occur due to neighboring words
+    s = Writing.remove_diar(s)
     s = Writing.archaicize_iota_subscript(s)
     s = Writing.phoneticize_breathing(s)
     s = Writing.accents_to_digraphs(s)
+    s = s.sub(/ς/,'σ')
     return s
   end
   def Writing.accents_to_digraphs(s)
@@ -35,6 +60,11 @@ class Writing
     s = s.tr('ᾳῃῳ','αηω')
     s = s.tr('ᾷῇῷ','ᾶῆῶ')
     s = s.tr('ᾴῄῴ','άήώ')
+    return s
+  end
+  def Writing.remove_diar(s)
+    # doesn't handle uppercase, grave accents, breathing
+    s = s.tr('ϊϋΐΰ','ιυίύ')
     return s
   end
   def Writing.phoneticize_breathing(s)
