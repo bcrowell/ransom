93c702887ad823d8cd9fc4653595a7bda9caad5d
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-01-23 18:10:04 -0800

    trying to get line numbering working for prose

--- eruby_ransom.rb
+++ eruby_ransom.rb
@@ -175,15 +175,21 @@ class WhereAt
       \\immediate\\openout\\posoutputfile=#{WhereAt.file_path}
     }
   end
+  def WhereAt.latex_code_to_close_pos_file()
+    return %q{
+      \closeout\posoutputfile
+    }
+  end
   def WhereAt.get_pos_data(line_hash,word_key)
     # returns a hash whose keys are "x","y","width","height","depth", all in units of pts
     return @@pos[[line_hash,word_key].join(",")]
   end
   def WhereAt.read_back_pos_file()
     @@pos = {} # will be a hash of hashes, @@pos[gloss_key][name_of_datum]
-    IO.foreach(WhereAt.file_path) { |line| 
+    IO.foreach(WhereAt.file_path) { |line|
+      next if line=~/^\?/
       line.sub!(/\s+$/,'') # trim trailing whitespace, such as a newline
-      a = line.split(/,/,-1)
+      a = line.split(/;/,-1)
       line_hash,page,line,word_key,x,y,width,height,depth = a
       key = [line_hash,word_key].join(",")
       data = [x,y,width,height,depth]
@@ -204,10 +210,14 @@ class WhereAt
     # We write a separate text file such as iliad.pos, which records the position of each word on the ransom-note page.
     # This allows us, on a later pass, to place the glosses at the correct positions.
     # format of .pos file:
-    #     unique hash                     ,page,line,lemma,x,y,width,height,depth
-    #     9d8e0859efef6dc6d2d23419e0de8e7a,7,0,μηνις,,,31.32986pt,8.7386pt,2.80527pt
-    #     9d8e0859efef6dc6d2d23419e0de8e7a,7,0,μηνις,3789043,36515889,,,
-    #     Here line is a 0-based count from the top of the page.
+    #     unique hash                     ;page;line;lemma;x;y;width;height;depth;extra1;extra2
+    #     9d8e0859efef6dc6d2d23419e0de8e7a;7;0;μηνις;;;31.32986pt;8.7386pt;2.80527pt;;
+    #     9d8e0859efef6dc6d2d23419e0de8e7a;7;0;μηνις;3789043;36515889;;;;;
+    #     Here line is a 0-based count from the top of the page, if the text is in verse.
+    #     Lines starting with ? are ignored.
+    #     The extra1 and extra2 strings are JSON hashes designed to make it convenient to add enhancements to this system.
+    #     Extra1 is written using \immediate\write, extra2 using \write. Even if they have some of the same latex source code,
+    #     what gets written to the .pos file can be different because it depends on whether we use \immediate.
     # Inputs:
     #   word = the word that is actually going to be typeset on the page
     #   lemma_key = a convenient key, which is normally the lemma for the word, stripped of accents
@@ -216,14 +226,20 @@ class WhereAt
     # We obtain the (x,y) and (w,h,d) information at different points, and therefore we have two separate lines, each of them
     # containing one part of the information.
     # Re the need for \immediate in the following, see https://tex.stackexchange.com/q/604110/6853
-    code = %q{\savebox{\myboxregister}{WORD}%
+    code = %q(\savebox{\myboxregister}{WORD}%
       \makebox{\pdfsavepos\usebox{\myboxregister}}%
-      \immediate\write\posoutputfile{LINE_HASH,\thepage,LINE,KEY,,,\the\wd\myboxregister,\the\ht\myboxregister,\the\dp\myboxregister}%
-      \write\posoutputfile{LINE_HASH,\thepage,LINE,KEY,\the\pdflastxpos,\the\pdflastypos,,,}}
+      \immediate\write\posoutputfile{LINE_HASH;\thepage;LINE;KEY;;;\the\wd\myboxregister;\the\ht\myboxregister;\the\dp\myboxregister;EXTRA;}%
+      \write\posoutputfile{LINE_HASH;\thepage;LINE;KEY;\the\pdflastxpos;\the\pdflastypos;;;;EXTRA}%
+    )
+    extra_data = %q({"otherline":OTHERLINE})
+    extra_data.gsub!(/OTHERLINE/,%q(\arabic{linenumber}))
+    # ... from lineno package, my attempt to get line numbering to work for prose, where it's latex that's in charge of the line breaks.
+    #     In \immediate\write, this seems to give a line number that is lower by 1 than in \write, so that the values in extra1 and extra2 will differ.
     code.gsub!(/LINE_HASH/,line_hash)
     code.gsub!(/WORD/,word)
     code.gsub!(/LINE/,line_number.to_s)
     code.gsub!(/KEY/,lemma_key)
+    code.gsub!(/EXTRA/,extra_data)
     return code
   end
 end
@@ -678,11 +694,7 @@ class Init
 end
 
 END {
-  if Options.if_write_pos then
-    print %q{
-      \closeout\posoutputfile
-    }
-  end
+  if Options.if_write_pos then print WhereAt.latex_code_to_close_pos_file() end
   print %q{
     \end{document}
   }
