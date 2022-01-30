64f26fffddf347e67fae437d19e0772c879a95b8
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-01-30 13:50:10 -0800

    cleanup

--- eruby_ransom.rb
+++ eruby_ransom.rb
@@ -79,7 +79,7 @@ def print_four_page_layout_latex_helper(db,bilingual,next_layout,vl,core,start_c
   print bilingual.translation_text
   # https://tex.stackexchange.com/a/308934
   layout_for_illustration = next_layout  # place illustration at bottom of page coming immediately before the *next* four-page layout
-  if !layout_for_illustration.nil? then print do_illustration(layout_for_illustration) end
+  if !layout_for_illustration.nil? then print Illustrations.do_one(layout_for_illustration) end
 end
 
 def four_page_layout_vocab_helper(bilingual,genos,db,wikt,core,treebank,freq_file,notes,vocab_by_chapter,start_chapter,ch)
@@ -95,57 +95,6 @@ def four_page_layout_vocab_helper(bilingual,genos,db,wikt,core,treebank,freq_fil
     vocab_by_chapter = []
   end
   return core,vl,vocab_by_chapter
-end
-
-def do_illustration(layout)
-  # input layout may be the *next* layout if we're putting each illustration at the end of the one before the layout it represents
-  # FIXME -- This contains lots of hardcoded numbers, styling, and layout info that should be in the class file or somewhere else.
-  # FIXME -- It would be better to do this by writing the actual available space to a file and then reading it in on the next pass. The
-  #          method used here is only approximate.
-  from,to = layout.foreign_linerefs
-  result = ''
-  count = 0
-  Illustrations.list_of.each { |ill|
-    book,line,filename,width,height,label = ill
-    lineref = [book,line]
-    if (from<=>lineref)<=0 && (lineref<=>to)<=0 then
-      if count>=1 then
-        $stderr.print "WARNING: layout for #{layout.foreign_linerefs} contained more than one illustration, only one was used\n"
-        next
-      end
-      landscape = Illustrations.is_landscape(label)
-      if landscape then
-        w_in = 4.66 # FIXME -- hardcoded
-        if !Illustrations.reduced_width(label).nil? then w_in=Illustrations.reduced_width(label) end
-        pts_per_in = 72.0
-        margin = 0.5 # need this much space in inches between translation and image
-        height_needed = (w_in*(height.to_f/width.to_f)+margin)*pts_per_in
-        width_latex_code = "#{w_in}in"
-      else
-        height_needed = 3.5 # inches
-        width_latex_code = (height_needed*(width.to_f/height.to_f)).to_s+"in"
-      end
-      foreign = layout.foreign
-      if Illustrations.hand_written_caption(label).nil? then
-        caption = foreign.extract(foreign.line_to_hard_ref(lineref[0],lineref[1]),foreign.line_to_hard_ref(lineref[0],lineref[1]+1))
-        caption = caption.gsub(/\n/,' ')
-      else
-        caption = Illustrations.hand_written_caption(label)
-      end
-      caption = "\n\n\\hfill{}\\linenumber{#{book}.#{line}}\\hspace{3mm} "+caption+"\n"
-      info = "#{filename}, height_needed=#{height_needed} in"
-      x = %q{
-        \vfill
-        % illustration and caption, __INFO__
-        \edef\measurepage{\the\dimexpr\pagegoal-\pagetotal-\baselineskip\relax}
-        \ifdim\measurepage > __MIN_HT__pt \hfill\includegraphics[width=__WIDTH__]{__FILE__}__CAPTION__ \else \fi \relax
-      }
-      result += x.gsub(/__FILE__/,filename).gsub(/__MIN_HT__/,height_needed.to_s).gsub(/__CAPTION__/,caption).gsub(/__INFO__/,info). \
-            gsub(/__WIDTH__/,width_latex_code)
-      count +=1
-    end
-  }
-  return result
 end
 
 def header_latex(bilingual)
--- lib/illustrations.rb
+++ lib/illustrations.rb
@@ -1,5 +1,7 @@
 =begin
 This needs to be loaded after the Options class has already been set up.
+
+FIXME: Contains hard-coded dimensions in Illustrations.do_one().
 =end
 
 class Illustrations
@@ -48,4 +50,56 @@ class Illustrations
     }
     return result.join(". ")+"."
   end
+
+def Illustrations.do_one(layout)
+  # input layout may be the *next* layout if we're putting each illustration at the end of the one before the layout it represents
+  # FIXME -- This contains lots of hardcoded numbers, styling, and layout info that should be in the class file or somewhere else.
+  # FIXME -- It would be better to do this by writing the actual available space to a file and then reading it in on the next pass. The
+  #          method used here is only approximate.
+  from,to = layout.foreign_linerefs
+  result = ''
+  count = 0
+  Illustrations.list_of.each { |ill|
+    book,line,filename,width,height,label = ill
+    lineref = [book,line]
+    if (from<=>lineref)<=0 && (lineref<=>to)<=0 then
+      if count>=1 then
+        $stderr.print "WARNING: layout for #{layout.foreign_linerefs} contained more than one illustration, only one was used\n"
+        next
+      end
+      landscape = Illustrations.is_landscape(label)
+      if landscape then
+        w_in = 4.66 # FIXME -- hardcoded
+        if !Illustrations.reduced_width(label).nil? then w_in=Illustrations.reduced_width(label) end
+        pts_per_in = 72.0
+        margin = 0.5 # need this much space in inches between translation and image
+        height_needed = (w_in*(height.to_f/width.to_f)+margin)*pts_per_in
+        width_latex_code = "#{w_in}in"
+      else
+        height_needed = 3.5 # inches
+        width_latex_code = (height_needed*(width.to_f/height.to_f)).to_s+"in"
+      end
+      foreign = layout.foreign
+      if Illustrations.hand_written_caption(label).nil? then
+        caption = foreign.extract(foreign.line_to_hard_ref(lineref[0],lineref[1]),foreign.line_to_hard_ref(lineref[0],lineref[1]+1))
+        caption = caption.gsub(/\n/,' ')
+      else
+        caption = Illustrations.hand_written_caption(label)
+      end
+      caption = "\n\n\\hfill{}\\linenumber{#{book}.#{line}}\\hspace{3mm} "+caption+"\n"
+      info = "#{filename}, height_needed=#{height_needed} in"
+      x = %q{
+        \vfill
+        % illustration and caption, __INFO__
+        \edef\measurepage{\the\dimexpr\pagegoal-\pagetotal-\baselineskip\relax}
+        \ifdim\measurepage > __MIN_HT__pt \hfill\includegraphics[width=__WIDTH__]{__FILE__}__CAPTION__ \else \fi \relax
+      }
+      result += x.gsub(/__FILE__/,filename).gsub(/__MIN_HT__/,height_needed.to_s).gsub(/__CAPTION__/,caption).gsub(/__INFO__/,info). \
+            gsub(/__WIDTH__/,width_latex_code)
+      count +=1
+    end
+  }
+  return result
+end
+
 end
