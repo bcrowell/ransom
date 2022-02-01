56a544b7dc457e0ea2e183f2b511676ba9890a12
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-02-01 15:02:38 -0800

    fix bug in scripts/check_glosses.rb

--- scripts/check_glosses.rb
+++ scripts/check_glosses.rb
@@ -10,19 +10,24 @@ require_relative "../lib/string_util"
 
 count = 0
 count_errs = 0
-db = GlossDB.from_genos(GreekGenos.new('epic'))
-Dir.glob(['glosses/*','glosses/_latin/*']).each { |filename|
-  next if filename=~/~/
-  next if filename=~/README/
-  filename=~/(([[:alpha:]]|_)+)$/
-  key = $1
-  next if key=~/^_/
-  count += 1
-  err,message = Gloss.validate(db,key)
-  if err then
-    print "error in file #{key}\n  ",message,"\n"
-    count_errs += 1
-  end
+
+greek_db = GlossDB.from_genos(GreekGenos.new('epic'))
+latin_db = GlossDB.from_genos(Genos.new('la'))
+[['glosses/*',greek_db],['glosses/_latin/*',latin_db]].each { |x|
+  glob,db = x
+  Dir.glob(glob).each { |filename|
+    next if filename=~/~/
+    next if filename=~/README/
+    filename=~/(([[:alpha:]]|_)+)$/
+    key = $1
+    next if key=~/^_/
+    count += 1
+    err,message = Gloss.validate(db,key)
+    if err then
+      print "error in file #{key}\n  ",message,"\n"
+      count_errs += 1
+    end
+  }
 }
 if count_errs>0 then $stderr.print "Failing after #{count_errs} errors.\n"; exit(-1) end
 print "checked #{count} keys\n"
