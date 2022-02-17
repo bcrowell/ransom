a8741eac589176bee9c86bea3c2eb22eebaf1ca4
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-02-16 17:04:31 -0800

    split module

---
+++ greek/find_all_particles.rb
@@ -0,0 +1,80 @@
+require 'json'
+require_relative "../lib/file_util"
+require_relative "../lib/string_util"
+require_relative "../lib/treebank"
+
+
+=begin
+
+σέθεν
+  "σέθεν": [
+    "σύ",
+    "1",
+    "p-s----g-",
+    22,
+    true,
+    [
+      [
+        "σύ",
+        "1",
+        "p-s----g-",
+        22
+      ],
+      [
+        "σύ",
+        "",
+        "p-s---mg-",
+        4
+      ]
+    ]
+  ],
+
+=end
+
+desired_pos_tag = 'g'
+
+x = TreeBank.new('homer').lemmas
+
+a = []
+x.each { |word,data|
+  lemma,glub1,pos,glub2,ambig,stuff = data
+  part_of_speech,person,number,gender,c = pos[0],pos[1],pos[2],pos[6],pos[7]
+  next if word=~/᾽/
+  next if (part_of_speech!=desired_pos_tag)  && !ambig
+  is_desired_pos = false
+  if stuff.nil? then
+    # data=["ἀτάρ", "", "g--------", 11, false, nil]
+    is_desired_pos = (data[2][0]==desired_pos_tag)
+  else
+    stuff.each { |lem| if lem[2][0]==desired_pos_tag then is_desired_pos=true end }
+  end
+  next if !is_desired_pos
+  word = grave_to_acute(word) # don't double count
+  # A word can have both an acute and a grave, and then this converts it into a word with two acutes, which is bogus.
+  n_acute = 0
+  word.chars.each { |c| if remove_acute_and_grave(c)!=c then n_acute +=1 end }
+  next if n_acute>1
+  word='ἦ' if lemma=='ἤ' # occurs because it's ambiguous, lemma can be ἦ for the word stripped of accents
+  a.push(word)
+}
+a = a.uniq
+
+# When these particles have both accented and unaccented forms, the only difference is that the accented one is
+# used for emphasis. Delete the accented ones, which are less common.
+mark_for_deletion = []
+0.upto(a.length-1) { |i|
+  0.upto(a.length-1) { |j|
+    next if i==j
+    mark_for_deletion.push(i) if remove_acute_and_grave(a[i])==a[j]
+  }
+}
+b = []
+0.upto(a.length-1) { |i|
+  b.push(a[i]) unless mark_for_deletion.include?(i)
+}
+a = b
+
+print a.length,"\n"
+print alpha_sort(a).join(" ")
+
+
--- lib/vocab_page.rb
+++ lib/vocab_page.rb
@@ -71,8 +71,8 @@ def VocabPage.make_helper(db,commonness,vl,lo,hi,core)
       this_sec += "\\begin{#{envir}}\n"
       ll.sort { |a,b| alpha_compare(a[4],b[4])}.each { |entry|
         s = nil
-        if type=='gloss' then s=VocabPage.entry(db,entry) end
-        if type=='conjugation' || type=='declension' then s=VocabPage.inflection(entry) end
+        if type=='gloss' then s=FormatGloss.with_english(db,entry) end
+        if type=='conjugation' || type=='declension' then s=FormatGloss.inflection(entry) end
         if !(s.nil?) then
           this_sec += clean_up_unicode("#{s}\n")
         else
@@ -90,77 +90,5 @@ def VocabPage.make_helper(db,commonness,vl,lo,hi,core)
   end
 end
 
-def VocabPage.entry(db,stuff)
-  file_under,word,lexical,data = stuff
-  entry = Gloss.get(db,lexical)
-  return if entry.nil?
-  preferred_lex = entry['word']
-  # ...If there is a lexical form used in the database (such as Perseus), but we want some other form (such as Homeric), then
-  #    preferred_lex will be different from the form inside stuff.
-  word2,gloss,lexical2 = entry['word'],entry['gloss'],entry['lexical']
-  if is_feminine_ending_in_os(remove_accents(lexical)) then gloss = "(f.) #{gloss}" end
-  explain_inflection = false
-  flags = {}
-  ['is_3rd_decl','is_dual','is_irregular_comparative'].each { |flag|
-    if (data.has_key?(flag) && data[flag] && !alpha_equal(word,lexical)) then
-      explain_inflection = true
-      flags[flag] = true
-    end
-  }
-  is_3rd_decl,is_dual,is_irregular_comparative = [flags['is_3rd_decl']==true,flags['is_dual']==true,flags['is_irregular_comparative']==true]
-  explain_inflection ||= data['difficult_to_recognize']
-  # Count chars, and if it looks too long to fit on a line, switch to the short gloss:
-  if explain_inflection then
-    text = [word.downcase,preferred_lex,gloss]
-  else
-    text = [preferred_lex,gloss]
-  end
-  total_chars = text.map { |t| t.length}.sum+text.length-1 # final terms count blanks
-  if total_chars>35 && entry.has_key?('short') then gloss=entry['short'] end
-  has_mnemonic_cog = entry.has_key?('mnemonic_cog')
-  # Generate latex:
-  inflected = LemmaUtil.make_inflected_form_flavored_like_lemma(word)
-  # FIXME: The explainer doesn't actually get printed for θᾶσσον ≺ ταχύς in Ilid 2.440.
-  explained = gloss+VocabPage.explainer_in_gloss(inflected,flags,data['pos'])
-  if !has_mnemonic_cog then
-    if explain_inflection then
-      s = "\\vocabinflection{#{inflected}}{#{preferred_lex}}{#{explained}}"
-    else
-      s = "\\vocab{#{preferred_lex}}{#{explained}}"
-    end
-  else
-    if explain_inflection then
-      s = "\\vocabinflectionwithcog{#{inflected}}{#{preferred_lex}}{#{explained}}{#{entry['mnemonic_cog']}}"
-    else
-      s = "\\vocabwithcog{#{preferred_lex}}{#{explained}}{#{entry['mnemonic_cog']}}"
-    end
-  end
-  return s
-end
-
-def VocabPage.explainer_in_gloss(word,flags,pos)
-  is_3rd_decl,is_dual,is_irregular_comparative = [flags['is_3rd_decl']==true,flags['is_dual']==true,flags['is_irregular_comparative']==true]
-  explainer = nil
-  explainer = 'dual' if is_dual
-  explainer = describe_declension(pos,true)[1] if is_3rd_decl
-  # ... here the is_3rd_decl flag just means it's a hard declension, could be stuff like -φιν
-  explainer = 'comparative' if is_irregular_comparative
-  if explainer.nil? then return '' else return ", #{explainer}" end
-end
-
-def VocabPage.inflection(stuff)
-  file_under,word,lexical,data = stuff
-  lemma_flavored = LemmaUtil.make_inflected_form_flavored_like_lemma(word)
-  pos = data['pos']
-  if pos[0]=='n' then
-    return "\\vocabnouninflection{#{lemma_flavored}}{#{lexical}}{#{describe_declension(pos,true)[0]}}"
-  end
-  if pos[0]=~/[vt]/ then
-    # File.open("debug.txt",'a') { |f| f.print "          #{word} #{lexical} #{pos} \n" }
-    return "\\vocabverbinflection{#{word.downcase}}{#{lexical}}{#{Vform.new(pos).to_s_fancy(tex:true,relative_to_lemma:lexical,
-                   omit_easy_number_and_person:true,omit_voice:true)}}"
-  end
-  return "\\vocabinflectiononly{#{word.downcase}}{#{lexical}}"
-end
 
 end
