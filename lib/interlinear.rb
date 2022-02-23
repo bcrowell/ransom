51cc30b3b2685ab043492bcf75cb3e27b21e5ecb
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-02-23 06:31:56 -0800

    code cleanup

--- lib/interlinear.rb
+++ lib/interlinear.rb
@@ -39,9 +39,14 @@ class InterlinearStyle
     @layout = layout
     @format = format
     @left_margin = left_margin
+    # style and estimation for proportional fonts:
+    @prop_p = 1.8  # average width of a character, in millimeters; (meas of default Latin font in ransom.cls gives more like 1.9 mm)
+    @prop_gloss_size = 'footnotesize' # also tried small
+    @prop_gloss_q = 0.55 # if font size is small, should be more like 0.6; used for estimating size of glosses in col_width_helper_proportional
+    @prop_max_total_width = 98.0 # millimeters; this value is about right for a 6"x9" book
   end
 
-  attr_accessor :layout,:format,:left_margin
+  attr_accessor :layout,:format,:left_margin,:prop_gloss_size,:prop_gloss_q,:prop_p,:prop_max_total_width
 
 end
 
@@ -97,11 +102,9 @@ def Interlinear.assemble_one_line(foreign_genos,words,style:InterlinearStyle.new
   end
   if format=='tex' then
     have_left_margin = (left_margin[0]>0)
-    max_total_width = 98.0 # in millimeters; FIXME: shouldn't be hardcoded
-    if have_left_margin then
-      max_total_width -= 1.8*left_margin[0] # FIXME: hardcoded coefficient in mm
-    end
-    widths = Interlinear.col_width_helper_proportional(table,n_rows,n_cols,layout,max_total_width)
+    max_total_width = style.prop_max_total_width
+    if have_left_margin then max_total_width -= style.prop_p*left_margin[0] end
+    widths = Interlinear.col_width_helper_proportional(style,table,n_rows,n_cols,layout,max_total_width)
     width_string = widths.map { |x| "p{#{x.round(2)}mm}" }.join('')
     if have_left_margin then
       width_string='l'+width_string
@@ -122,7 +125,7 @@ def Interlinear.assemble_one_line(foreign_genos,words,style:InterlinearStyle.new
         if layout[row]=~/[p]/ then is_pos=true else is_pos=false end
         if layout[row]=~/[g]/ then is_gloss=true else is_gloss=false end
         if is_pos then e = e.gsub(/([A-Z]+)/) {"{\\scriptsize #{$1}}"} end
-        if is_gloss then e = "{\\small #{e}}" end
+        if is_gloss then e = "{\\#{style.prop_gloss_size} #{e}}" end
         if foreign_genos.greek && is_foreign then e="\\begin{greek}\\large #{e}\\end{greek}" end
         elements.push(e)
       }
@@ -130,22 +133,26 @@ def Interlinear.assemble_one_line(foreign_genos,words,style:InterlinearStyle.new
     }
     result += lines.join("\\\\\n")+"\n"
     result += %q(\end{tabular})+"\n"
+    result = "{\\renewcommand{\\arraystretch}{1.1}\\setstretch{0.7}\n#{result}\\par}"
+    # Setstretch is from package setspace; \par is necessary; https://tex.stackexchange.com/questions/83855/change-line-spacing-inside-the-document
+    # The arraystretch is to add more space between rows, compensating for the setstretch.
+    # Both of these effects are localized to the group formed by the outer {}.
     return result
   end
   raise "format #{format} not implemented"
 end
 
-def Interlinear.col_width_helper_proportional(table,n_rows,n_cols,layout,max_total_width,max_gloss_lines:3)
+def Interlinear.col_width_helper_proportional(style,table,n_rows,n_cols,layout,max_total_width,max_gloss_lines:3)
   widths = nil
   1.upto(max_gloss_lines) { |n|
     widths = []
     0.upto(n_cols-1) { |col|
       cell_widths = []
-      p = 1.8 # average width of a character, in millimeters; (meas of default Latin font in ransom.cls gives more like 1.9 mm)
+      p = style.prop_p # average width of a character, in millimeters
       0.upto(n_rows-1) { |row|
         what = layout[row]
-        q = 1.0 # unitless coefficient that basically represents the average font size
-        if what=='g' then q=0.6 end # glosses are in a slightly smaller font, {\small ...}
+        q = 1.0 # unitless coefficient that basically represents the font size, compared to the main font
+        if what=='g' then q=style.prop_gloss_q end
         n_chars = table[col][row].length
         if what=='g' then b=n_chars.to_f/n else b=n_chars end
         cell_widths.push(q*b)
