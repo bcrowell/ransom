604ff80c2bde0c25a8574ad3ebc81915b3bb0823
Ben Crowell <notreal@fakewillnotwork.invalid>
2022-02-28 16:36:37 -0800

    work on Spew

--- lib/spew.rb
+++ lib/spew.rb
@@ -7,35 +7,43 @@ class Spew
     @s = s
   end
 
+  def like(s)
+    return Spew.new(@format,s) # makes a new Spew object with the same format but a different string
+  end
+
   def to_s
-    return @s
+    s = @s
+    s = s.gsub(/<([^>]*)>/) {self.like($1).underline}
+    unless @format=='tex' then s=s.gsub(/\\ldots/,'...') end
+    return s
   end
 
   def bold
     new_s = @s.dup
     if @format=='tex'    then new_s = Latex.macro('textbf',self.to_s) end
     if @format=='bbcode' then new_s = BBCode.bold(self.to_s) end
-    return Spew.new(@format,new_s)
+    return self.like(new_s)
   end
 
   def italic
     new_s = @s.dup
     if @format=='tex'    then new_s = Latex.macro('textit',self.to_s) end
     if @format=='bbcode' then new_s = BBCode.italic(self.to_s) end
-    return Spew.new(@format,new_s)
+    return self.like(new_s)
   end
 
   def underline
     new_s = @s.dup
     if @format=='tex'    then new_s = Latex.macro('underline',self.to_s) end
     if @format=='bbcode' then new_s = BBCode.underline(self.to_s) end
-    return Spew.new(@format,new_s)
+    return self.like(new_s)
   end
 
   def pre # preformatted text
     new_s = @s.dup
     if @format=='tex'    then new_s = Latex.envir('verbatim',self.to_s) end
     if @format=='bbcode' then new_s = BBCode.pre(self.to_s) end
-    return Spew.new(@format,new_s)
+    return self.like(new_s)
   end
+
 end
